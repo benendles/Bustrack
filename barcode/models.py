d47ef1cd8960a0c4506d28d57ac534a3e344d677
bernedless <fonyuybernard257@gmail.com>
2025-06-28 12:08:09 +0100

    maybe last update

--- barcode/models.py
+++ barcode/models.py
@@ -2,28 +2,18 @@ from django.db import models
 from io import BytesIO
 from django.core.files.base import ContentFile
 import qrcode
-from django.conf import settings
-from django.urls import reverse
 
 class Website(models.Model):
     name = models.CharField(max_length=100)
     amount = models.DecimalField(max_digits=10, decimal_places=2, default=0.00)
-    receipt_image = models.ImageField(upload_to='receipt_images/',blank=True,null=True)  # Receipt image
-    picture = models.ImageField(upload_to='user_pictures/',blank=True,null=True)           # User's own picture
+    receipt_image = models.ImageField(upload_to='receipt_images/', blank=True, null=True)
+    picture = models.ImageField(upload_to='user_pictures/', blank=True, null=True)
     qr_code = models.ImageField(upload_to='qr_codes', blank=True, null=True)
 
     def __str__(self):
         return f"Extracted Image {self.id} - {self.name}"
 
-   
-class Website(models.Model):
-    ...
-
     def generate_qr(self, url):
-        import qrcode
-        from io import BytesIO
-        from django.core.files.base import ContentFile
-
         qr_img = qrcode.make(url)
         qr_img = qr_img.convert('RGB')
         buffer = BytesIO()

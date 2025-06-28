6f27205fb404fcd4ded3174aea9f6f0dc79b9eff
bernedless <fonyuybernard257@gmail.com>
2025-06-28 10:51:02 +0100

    deleting

--- source/barcode copy/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import Website
-
-class WebsiteForm(forms.ModelForm):
-    class Meta:
-        model = Website
-        fields = ['name', 'amount', 'receipt_image', 'picture']
--- source/barcode copy/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.submit_info, name='submit_info'),
-    path('qr/<int:pk>/', views.show_qr, name='show_qr'),
-    path('info/<int:pk>/', views.view_info, name='view_info'),
-]
